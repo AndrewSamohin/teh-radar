5609d0e5b400adf3b11c7404b0a78840e3b08b19
Andrew Samohin <andreusamokhin@mail.ru>
2025-06-22 21:16:07 +0300

    Creating a project

---
+++ src/main/java/com/example/teh_radar/TehRadarApplication.java
@@ -0,0 +1,13 @@
+package com.example.teh_radar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TehRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TehRadarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teh_radar/TehRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teh_radar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TehRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
